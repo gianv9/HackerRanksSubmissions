6c532eb26ff5573cd66fe51fcce822a73155abfe
gian <gianfrancoverrocchi@gmail.com>
2018-08-10 21:11:28 -0400

    Updated solition to: Detect HTML links

--- Regex/Detect HTML links/solution.py
+++ Regex/Detect HTML links/solution.py
@@ -2,18 +2,18 @@
 #   https://docs.python.org/3/howto/regex.html#grouping
 import re
 # this pattern finds the link
-pattern1 = r'(<a.*(?P<href>(?<=href=\")[^\s]*(?=\")).*>)(?P<data>.*)(<\/a>)'
-# (<a.*(?P<href>(?<=href=\")[^\s]*(?=\")).*>)(?P<data>.*)(<\/a>)
+pattern1 = r'<a href=\"(?P<href>.*?)\".*?>((<[a-z]>)?\s?(?P<data>.*?)(<\/[a-z]>)?)<'
 p1 = re.compile(pattern1)
 
 lines = int(input())
+html_lines = ""
 while lines > 0:
-    html_line = str(input())
-    result = p1.search(html_line)
-    # comma = "\n"
-    if result is not None:
-        print(result.group('href')+","+result.group('data'))
-    # comma = "\n" if comma == "," else ","
+    html_lines = html_lines + str(input())
     lines = lines - 1
 
-# see this regex in action: https://regex101.com/r/WGgJwy/2
+result = p1.findall(html_lines)
+
+for element in result:
+    print(element[0]+","+element[3])
+
+# # see this regex in action: https://regex101.com/r/WGgJwy/5
